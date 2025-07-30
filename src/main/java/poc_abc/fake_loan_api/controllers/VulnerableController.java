2b217e3f1e3761c5cf3441629e571d18b05ec9b0
Francisco Nascimento <francisco.nascimento@bradesco.com.br>
2025-07-30 13:03:13 -0300

    feat: Adicionando vulnerabilidade no codigo set build

---
+++ src/main/java/poc_abc/fake_loan_api/controllers/VulnerableController.java
@@ -0,0 +1,20 @@
+package poc_abc.fake_loan_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import poc_abc.fake_loan_api.utils.UnsafeEvaluator;
+
+@RestController
+@RequestMapping("/test")
+public class VulnerableController {
+
+    @GetMapping("/eval")
+    public String runEval(@RequestParam String code) throws Exception {
+        UnsafeEvaluator evaluator = new UnsafeEvaluator();
+        Object result = evaluator.evaluate(code);
+        return "Result: " + result;
+    }
+}
